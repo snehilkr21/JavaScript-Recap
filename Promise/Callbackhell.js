07c224c5ca162cf45ca362781cf8d64fdf7f9bda
snehil <snehil@snehils-MacBook-Air.local>
2025-07-25 11:57:36 +0530

    callbackhell, promise chaining

---
+++ Promise/Callbackhell.js
@@ -0,0 +1,20 @@
+const cart = ["kurta","trouser","shirt"]
+
+API.createOrder(cart,function(){
+    API.proceedToPayment(function(){
+        API.showOrderSUmmary(function(){
+            API.updateWallet()
+        })
+    })
+})
+
+createOrder(cart)
+.then(function(orderId){
+  return proceedToPayment(orderId)
+})
+.then(function(paymentId){
+    return orderSummary(paymentId)
+})
+.then(function(){
+   return updateWallet()
+})
