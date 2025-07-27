1c978dd154614adbf13d3c9553f76cc430bc15a7
snehil <snehil@snehils-MacBook-Air.local>
2025-07-27 17:35:14 +0530

    forEach polyfill

---
+++ Polyfill/ForEach.js
@@ -0,0 +1,9 @@
+const arr = [1, 2, 3, 4];
+Array.prototype.myForEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    console.log(this[i] + " " + callback(this[i]));
+  }
+};
+arr.myForEach((el) => {
+  return el * 2;
+});
